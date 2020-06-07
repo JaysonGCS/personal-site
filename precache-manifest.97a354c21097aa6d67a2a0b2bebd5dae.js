01d9452de7249306fcb3d1d6394bd94465d5e1c1
Jayson Goh Chung Sern <goh.chung.sern@u.nus.edu>
2020-06-07 18:30:02 +0800

    Updates

---
+++ precache-manifest.97a354c21097aa6d67a2a0b2bebd5dae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f4793018ab609c69772a6fe927226ae",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "b182dbb3432ceaeb471f",
+    "url": "/react-deploy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "169db92b2963a491015e",
+    "url": "/react-deploy/static/js/2.904c5842.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy/static/js/2.904c5842.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b182dbb3432ceaeb471f",
+    "url": "/react-deploy/static/js/main.645fc973.chunk.js"
+  },
+  {
+    "revision": "88cc752382718b1d2923",
+    "url": "/react-deploy/static/js/runtime-main.625baed9.js"
+  }
+]);
